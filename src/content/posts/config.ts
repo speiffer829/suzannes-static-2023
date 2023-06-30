9b2720e96f5a35466de90a85bf5aaa342dc1fef9
Spenser <spenser@minddnd.com>
2023-06-30 11:41:54 -0400

    blog is now posts

--- src/content/posts/config.ts
+++ src/content/posts/config.ts
@@ -1,14 +1,15 @@
 import { z, defineCollection } from 'astro:content';
 
-const blog_collection = defineCollection(() => {
+const posts_collection = defineCollection(() => {
 	schema: z.object({
 		title: z.string(),
 		date: z.string(),
+		updated: z.string().optional(),
 		description: z.string().optional(),
 		img: z.string().optional()
 	});
 });
 
 export const collections = {
-	blog: blog_collection
+	posts: posts_collection
 };
