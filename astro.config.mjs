6975914635c346cdafbeaa0cd29c75d72558b49b
Spenser <spenser@minddnd.com>
2023-07-02 00:38:38 -0400

    fixed image typing (I think)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'http://suzannesdance.com',
-  integrations: [tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap(), mdx()],
-  experimental: {
-    assets: true
-  }
+	site: 'http://suzannesdance.com',
+	integrations: [tailwind(), sitemap(), mdx()],
+	experimental: {
+		assets: true
+	}
 });
